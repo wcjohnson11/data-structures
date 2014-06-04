246e2a85082974b94945e936c4fa2f7b2421c06b
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-04 15:44:54 -0700

    Finished Queue, would like to make size prettier

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,34 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var qStart = 0;
+  var qEnd = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    //Add string to storage[size]
+    storage[qEnd] = value;
+    //increment qEnd
+    qEnd++;
   };
 
   someInstance.dequeue = function(){
+    //store the value of storage[0] in a var
+    var result = storage[qStart];
+    //delete oldest stored value
+    delete storage[qStart];
+
+    if((qEnd - qStart) > 0){
+      qStart++;
+    }
+    //return vaue
+    return result;
   };
 
   someInstance.size = function(){
+    return (qEnd - qStart);
   };
 
   return someInstance;
