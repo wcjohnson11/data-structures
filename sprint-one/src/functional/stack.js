d2c45600d1785a1f02cb07a48ffd428d527e2547
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-04 14:58:40 -0700

    Committing stack and pseudocode for queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    //store value in storage object at a key of size
+    storage[size] = value;
+    //increment size
+    size++;
   };
 
   someInstance.pop = function(){
+  //decrement size
+    if(size >0){
+      size--;
+    }
+  //store value from storage[size] and then return it
+    var result = storage[size];
+  //delete stored value at storage[size]
+    delete storage[size];
+  //return stored value
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
