04078dac58f3380da86068c0e3a3e80f6287eb01
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-05 10:55:38 -0700

    Finished Pseudoclassical, on to profiling

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //create a storage object
+  //create qStart and  qEnd
+  this._storage = {};
+  this._qStart = 0;
+  this._qEnd = 0;
 };
 
+//add new methods to queue, enqueue, will take a value
+//store the value @ storage[qStart]
+//increment qStart
+Queue.prototype.enqueue = function(value){
+  this._storage[this._qStart] = value;
+  this._qStart++;
+};
 
+//add a new method,dequeue
+//store storage[qEnd] in a result var
+//delete stor[qEnd]
+//increment qEnd
+////return result
+Queue.prototype.dequeue = function(){
+  var result = this._storage[this._qEnd];
+  delete this._storage[this._qEnd];
+  (this._qStart - this._qEnd) && this._qEnd ++;
+  return result;
+};
+
+//add a method size,qStart - qEnd
+Queue.prototype.size = function(){
+  return this._qStart - this._qEnd;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //Create a storage var
+  //create size var
+  this._storage = {};
+  this._size = 0;
 };
 
+//create pop method
+//create result var and store storage[size]
+//delet storage size
+//if size>0 decrement size
+//return result
+Stack.prototype.pop = function(){
+  if(this._size > 0){
+    this._size--;
+  }
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
+  return result;
+};
 
+//take in a value
+//store the value at storage[size]
+//increment size
+Stack.prototype.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+//return size
+Stack.prototype.size = function(){
+  return this._size;
+};
