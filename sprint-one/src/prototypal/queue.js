d001a833278fba382b5c821e65895acbc5cf4793
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-04 21:23:50 -0700

    Finished prototypal like it ain't no thing

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,46 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create a new variable and store the result of object.create with the argument of queue methods
+  // assign storage
+  // assign qStart
+  // assign qEnd
+  // return object
+
+  var newQueue = Object.create(queueMethods);
+  newQueue._storage = {};
+  newQueue._qStart = 0;
+  newQueue._qEnd = 0;
+  return newQueue;
+
 };
 
 var queueMethods = {};
 
 
+//enqueue, takes a single value arg
+//store the value at storage[qstart]
+//increment qstart
+queueMethods.enqueue = function(value){
+  this._storage[this._qEnd] = value;
+  this._qEnd++;
+};
+//dequeue
+//create a result variable and store this._storage[this._qEnd]
+//if qEnd - qStart > 0 increment the qEnd
+//delete the dequeued
+//return the result
+queueMethods.dequeue = function(){
+  var result = this._storage[this._qStart];
+  delete this._storage[this._qStart];
+  if((this._qEnd - this._qStart) > 0){
+    this._qStart++;
+  }
+  return result;
+};
+
+//size
+//return qEnd - qStart
+queueMethods.size = function(){
+  return (this._qEnd - this._qStart);
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,48 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Store a newly created object from Object.create with stackMethods as the arg
+  // create a storage variable
+  // create a size variable
+  // return the object
+  var newObj = Object.create(stackMethods);
+  newObj._storage = {};
+  newObj._size = 0;
+  return newObj;
 };
 
 var stackMethods = {};
+
+//pop method
+//if size > 0, decrement size
+//store the value of storage at size
+//delete storage at size
+//return value
+
+stackMethods.pop = function(){
+  if (this._size > 0){
+    this._size--;
+  }
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
+  return result;
+};
+
+
+//push method, takes a value
+//stores it in storage at size
+//increment size
+stackMethods.push = function(value){
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+
+//size, returns size
+stackMethods.size = function(){
+  return this._size;
+};
+
 
 
