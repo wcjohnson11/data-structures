a5cdb49cda65618165601482dd9111244a457704
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-04 20:29:00 -0700

    Fixed the functional-shared enqueue typo, all passed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,7 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-
+  //
 //Create a new queue object
   var newQueue = {};
 
@@ -11,8 +11,10 @@ var makeQueue = function(){
   newQueue.qStart = 0;
   newQueue.qEnd = 0;
 //extend the queue methods onto the queue object
-  extend(newQueue, queueMethods);
-//return queue object
+
+  _.extend(newQueue, queueMethods);
+//return queue
+
   return newQueue;
 
 };
@@ -26,13 +28,13 @@ var extend = function(to, from){
 
 var queueMethods = {};
 
-queueMethods.enQueue = function(value){
-    //debugger;
+queueMethods.enqueue = function(value){
+
     this.storage[this.qEnd] = value;
     this.qEnd++;
   };
 
-queueMethods.deQueue = function(){
+queueMethods.dequeue = function(){
     var result = this.storage[this.qStart];
     delete this.storage[this.qStart];
     if((this.qEnd - this.qStart) > 0){
@@ -44,5 +46,4 @@ queueMethods.deQueue = function(){
 queueMethods.size = function(){
     return (this.qEnd - this.qStart);
   };
-
 
