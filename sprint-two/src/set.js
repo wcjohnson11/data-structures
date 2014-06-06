4485095b622fe0d0165d9e3534a3e8242cdab25a
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-05 20:50:17 -0700

    Hooray Underscore! Thanks for making set so easy

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //If the item isn't in storage, add item
+  if(!_.contains(this._storage, item)){
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+//Check whether item is in storage
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  //indexOf the item
+  //get RID OF IT with SPLICEEEE
+  var target = _.indexOf(this._storage, item);
+  (target >=0) && this._storage.splice(target, 1);
+
 };
 
 /*
