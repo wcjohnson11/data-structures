f1e762658ee5e42c2a164565c9794aacc18af6a9
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-05 20:14:38 -0700

    sayonara linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -14,23 +14,31 @@ var makeLinkedList = function(){
     //set list[value] to be list[tail]
     // list.tail = {};
     // list.tail.value = 4 ;
-
-
-debugger;
+    var node = makeNode(value);
     if(list.tail === null){
-      list.tail = value;
-      list.head = value;
+      list.tail = node;
+      list.head = node;
     } else {
 
-      list.tail.next = value;
-      list.tail = value;
+      list.tail.next = node;
+      list.tail = node;
     }
   };
 
   list.removeHead = function(){
+    //store list.head in a variable
+    //make list.head.next equal to list.head
+    //return the variable
+    var result = list.head;
+    list.head = list.head.next;
+    return result.value;
   };
 
   list.contains = function(target){
+    //return true if the target exists at value.value
+    return _.some(list, function(value){
+      return value.value === target;
+    });
   };
 
   return list;
