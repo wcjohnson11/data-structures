2a1cc16631fcfe227814c41651b48dfad7a4b647
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-05 21:58:28 -0700

    One prompt left for Hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,31 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //use limited array.set with i and v
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //Use limited array.get with i
+  return this._storage.get(i);
 
 };
 
 HashTable.prototype.remove = function(k){
+  //Store index of k at i
+  //Use limitedarray.each with our sweet callback
+  //make a callback function that checks for i at storage[index]
+  //if i === storage[index] set the value of storage[index] to null
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, index, array){
 
+    //
+    if(index === i){
+      array[index] = null;
+    }
+
+  });
 };
 
 
