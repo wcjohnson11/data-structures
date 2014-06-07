e65bb378f1376eb5041a4d9f8bf31f3d1ed1f18b
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-07 08:56:31 -0700

    Getting Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,20 +1,45 @@
 var Graph = function(){
-  this._storage = {};
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
-};
+  var keys = Object.keys(this.nodes);
+
+  //Store an object at this.nodes[newNode]
+  this.nodes[newNode] = {};
+  //if toNode, addEdge(newNode,toNode)
+  //store [toNode] to this.nodes[newNode] (this.nodes[newNode][toNode])
+  //also give this.nodes[todNode][newNode]
 
 Graph.prototype.contains = function(node){
+  //var keys = Object.keys(this.nodes);
+  //if _.contains(keys, node)
+  //return true
+
+  //OR this.nodes.hasOwnProperty(node);
+  return this.nodes.hasOwnProperty(node);
+
 };
 
 Graph.prototype.removeNode = function(node){
+  //use _.each to find node in this.nodes
+  //if found, delete this.nodes[node]
+  //use _.isEmpty(this.nodes[node])
+  //
+  //OR check this.nodes.hasOwnProperty(node)
+  //for each this.nodes.searchNode[edges]
+  //this.nodes.node[edge][searchNode]
+  //delete these nodes and check if node _.isEmpty
+  //if _.isEmpty, delete node
+  //FINALLY delete this.nodes[searchNode]
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  //An edge looks like this
+  this.nodes[fromNode][toNode];
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
